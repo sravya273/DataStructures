b9295c72e1d2cab204c43bca7cd1708cbca0a16c
Sravya <sravyavissamsetty@gmail.com>
2020-11-07 00:24:48 +0530

    level order tree traversal iterative
    https://www.geeksforgeeks.org/level-order-tree-traversal/

--- BinaryTree/levelOrder.cpp
+++ BinaryTree/levelOrder.cpp
@@ -1,62 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define pb push
+
 struct node 
 { 
     int data; 
     struct node* left;
 	struct node* right; 
 }; 
-struct node* newNode(int data) 
-{ 
-    struct node* node = (struct node*) 
-                        malloc(sizeof(struct node)); 
-    node->data = data; 
-    node->left = NULL; 
-    node->right = NULL; 
-  
-    return(node); 
-} 
-void levelOrder(queue<node* > levelqueue,node* head)
+
+node* newNode(int k)
 {
-	if(head!=NULL)
-	{
-		levelqueue.pb(head);
-		levelqueue.pb(newNode(0));
-		while(!levelqueue.empty())
-		{
-			node* temp = levelqueue.front();
-			
-			levelqueue.pop();
-			if(temp->data!=0)
-			{
-				cout<<temp->data<<" ";
-				if(temp->left!=NULL)
-				levelqueue.pb(temp->left);
-				
-				if(temp->right!=NULL)
-				levelqueue.pb(temp->right);
-				
-			}else if(temp->data==0 && !levelqueue.empty()){
-				levelqueue.pb(newNode(0));
-				cout<<endl;
-			}
-		}
-	}
+    struct node *temp = new node();
+    temp->data =k;
+    temp->left =NULL;
+    temp->right = NULL;
+    
+    return temp;
 }
-void printLevelOrder(node* head)
+void printLevelOrder(node* root)
 {
-	queue<node* > levelqueue;
-	
-	levelOrder(levelqueue,head);
+    queue <node*> q;
+    if(root!= NULL)
+    q.push(root);
+    node* temp;
+    while(!q.empty())
+    {
+        temp = q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+        
+        if(temp->left!= NULL)
+        q.push(temp->left);
+        
+        if(temp->right!=NULL)
+        q.push(temp->right);
+    }
 }
-
-
-
-
-  
-/* Driver program to test above functions*/
 int main() 
 { 
     struct node *root = newNode(1); 
